4f59f7b158db07c52b27c84632e45167b00ec81b
lzn <jannanlo@163.com>
2020-11-28 18:13:19 +0800

    fix code

--- lznSite/k8s/scripts/install-k8s-prepare.sh
+++ lznSite/k8s/scripts/install-k8s-prepare.sh
@@ -8,4 +8,7 @@ function ansibleCmd(){
 #ansible-playbook -i ./k8s-inventory  ./kube-install.yml --extra-vars "ansible_sudo_pass=123456"
 ansibleCmd "$*"
 
-# kubeadm init  --image-repository=docker.io/k8simgs
+# kubeadm init --config kubeadm.yaml 
+# mkdir -p $HOME/.kube 
+# sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config 
+# sudo chown $(id -u):$(id -g) $HOME/.kube/config 
